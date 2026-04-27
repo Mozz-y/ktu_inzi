372b641042486088fc5e6d10e9e8aafc18361fab
Vasarekrungleviciute <vasarekrungleviciute@gmail.com>
2026-04-27 20:39:19 +0300

    pridejau aktorius

---
+++ src/mocks/actor.ts
@@ -0,0 +1,17 @@
+export const mockActors: Actor[] = [
+  {
+    imageUrl: "https://media.themoviedb.org/t/p/w138_and_h175_face/7CIVAl4HiuWrmp4r8Jk09sqxTBy.jpg",
+    name: "Petras",
+    surname: "Petraitis"
+  },
+  {
+    imageUrl: "https://media.themoviedb.org/t/p/w138_and_h175_face/7CIVAl4HiuWrmp4r8Jk09sqxTBy.jpg",
+    name: "Petras",
+    surname: "Petraitis"
+  },
+  {
+    imageUrl: "https://media.themoviedb.org/t/p/w138_and_h175_face/7CIVAl4HiuWrmp4r8Jk09sqxTBy.jpg",
+    name: "Petras",
+    surname: "Petraitis"
+  }
+]
