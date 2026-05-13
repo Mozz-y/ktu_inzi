801b27464e04c644fffda563c452f17fbc19e7b4
elvinas jonyka <elvins.jons@gmail.com>
2026-05-13 10:38:46 +0300

    Added active session management. (Cant access home page, etc. if not logged in)

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,7 +1,46 @@
 import AppTabs from '@/components/app-tabs'; // Tavo meniu komponentas
-import React from 'react';
+import { supabase } from '@/lib/supabase';
+import { useRouter } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function Layout() {
-  // Šis layout'as uždės apatinį meniu TIK tiems failams, kurie guli (tabs) aplanke
+  const [isReady, setIsReady] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    let active = true;
+
+    const checkSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      if (!active) {
+        return;
+      }
+
+      if (!session) {
+        router.replace('/Login');
+      } else {
+        setIsReady(true);
+      }
+    };
+
+    checkSession();
+
+    return () => {
+      active = false;
+    };
+  }, [router]);
+
+  if (!isReady) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return <AppTabs />;
 }
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+// eslint-disable-next-line import/no-unresolved
 import { createClient } from '@supabase/supabase-js';
+// eslint-disable-next-line import/no-unresolved
 import 'react-native-url-polyfill/auto';
 
 export const supabase = createClient(
